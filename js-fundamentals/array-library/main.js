24699b4c31af33765ed54011701f4f6e97abe948
Angelina Valer'eva <mandaaarinka@mail.ru>
2021-04-12 18:30:17 +0300

    Implement functions n chainables methods

--- js-fundamentals/array-library/main.js
+++ js-fundamentals/array-library/main.js
@@ -1,34 +1,89 @@
 class ArrLib {
+    constructor(arr){
+        this.arr = arr;
+    }
+
+    static chain(arr){
+        return new ArrLib(arr); 
+    }
+
+    value(){
+        return this.arr;
+    }
+
+    take(n){
+        this.arr = Array.from(this.arr).slice(0, n);
+
+        return this;
+    }
+
+    skip(n){
+        this.arr = Array.from(this.arr).slice(n);
+
+        return this;
+    }
+
+    map(callback){
+        this.arr = ArrLib.map(this.arr, callback);
+
+        return this;
+    }
+
+    reduce(callback, initialValue){
+        this.arr = ArrLib.reduce(this.arr, callback, initialValue);
+
+        return this;
+    }
+
+    filter(callback){
+        this.arr = ArrLib.filter(this.arr, callback);
+
+        return this;
+    }
+
+    foreach(callback){
+        this.arr = ArrLib.foreach(this.arr, callback);
+
+        return this;
+    }
+
     static take(arr, n){
-        return Array.from(arr).slice(0, number);
+        return Array.from(arr).slice(0, n);
     }
 
     static skip(arr, n){
-        return Array.from(arr).slice(number);
+        return Array.from(arr).slice(n);
     }
 
     static map(arr, callback){
-        let res = [];
+        let result = [];
         for(let i = 0; i < arr.length; i++){
-            res.push(callback(arr[i], i, arr))
+            result.push(callback(arr[i], i, arr))
         }
+        return result;
     }
 
     static reduce(arr, callback, initialValue){
-        let value = [];
-        for(let i = 0; i < arr.length; i++){
-            value = callback(initialValue, arr[i], i, arr);
+        let res = initialValue;
+        for(let i = 0; i < arr.length; i++){ 
+            res = callback(res, arr[i], i, arr);
         }
+        return res;
     }
 
     static filter(arr, callback){
-        let res = [];
+        let result = [];
         for(let i = 0; i < arr.length; i++){
             if(callback(arr[i], i, arr)){
-                res.push(arr[i])
+                result.push(arr[i])
             }
         }
+        return result;
     }
 
-    static foreach(arr, callback){}
+    static foreach(arr, callback){
+        for(let i = 0; i < arr.length; i++){
+            callback(arr[i], i, arr);
+        }
+    }
 }
