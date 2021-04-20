a0e8a23ae0aaf1de3a58f30e09a4d2225124d9a0
Angelina Valer'eva <mandaaarinka@mail.ru>
2021-04-20 17:01:18 +0300

    Add extends property

--- js-code-style/shifter-mini-lib/.eslintrc.js
+++ js-code-style/shifter-mini-lib/.eslintrc.js
@@ -3,6 +3,9 @@ module.exports = {
         "browser": true,
         "es2021": true
     },
+    "extends": [
+        'airbnb-base',
+    ],
     "parserOptions": {
         "ecmaVersion": 12,
         "sourceType": "module"
