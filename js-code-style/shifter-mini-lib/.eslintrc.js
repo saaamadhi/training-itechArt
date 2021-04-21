b63a181c44e19cf16dcdf55a5b2680d0e1ec7056
Angelina Valer'eva <mandaaarinka@mail.ru>
2021-04-21 08:20:35 +0300

    Add eslint rules

--- js-code-style/shifter-mini-lib/.eslintrc.js
+++ js-code-style/shifter-mini-lib/.eslintrc.js
@@ -11,5 +11,26 @@ module.exports = {
         "sourceType": "module"
     },
     "rules": {
+        "no-plusplus": ["error", { "allowForLoopAfterthoughts": true }],
+        "no-tabs": ["error", { allowIndentationTabs: true }],
+        "func-names": ["error", "never"],
+        "no-param-reassign": ["error", { "props": true }],
+        "prefer-destructuring": ["error", { "object": true, "array": true }],
+        "camelcase": 0,
+        "no-use-before-define": ["error", { "functions": false, "variables": false }],
+        "prefer-const": ["error", {"ignoreReadBeforeAssign": true}],
+        "max-len": 0,
+        "no-mixed-spaces-and-tabs": 0,
+        "prefer-const": 0,
+        "no-var": 0,
+        "vars-on-top": 0,
+        "prefer-rest-params": 0,
+        "no-shadow": 0,
+        "no-restricted-syntax": 0,
+        "prefer-destructuring": 0,
+        "consistent-return": 0,
+        "no-continue": 0,
+        "no-nested-ternary": 0,
+        "no-undef": 0
     }
 };
