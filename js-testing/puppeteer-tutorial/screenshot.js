0204879ab5a731aa3e6c374984e65198ffca888f
Angelina Valer'eva <mandaaarinka@mail.ru>
2021-04-22 11:20:27 +0300

    Customize size of a new screenshot

--- js-testing/puppeteer-tutorial/screenshot.js
+++ js-testing/puppeteer-tutorial/screenshot.js
@@ -3,8 +3,13 @@ const puppeteer = require('puppeteer')
 const main = async () => {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
+  await page.setViewport({
+    width: 1920,
+    height: 1080,
+    deviceScaleFactor: 1,
+  })
   await page.goto('https://unsplash.com')
-  await page.screenshot({ path: 'unsplash.png' })
+  await page.screenshot({ path: 'unsplash2.png' })
 
   await browser.close()
 }
