d9ce1fbaa29145b9f41b3ddb240e281b1b51d4bd
Angelina Valer'eva <mandaaarinka@mail.ru>
2021-04-27 11:06:18 +0300

    Add base url, change require to import

--- js-testing/e2e-realworld-testing/logout.js
+++ js-testing/e2e-realworld-testing/logout.js
@@ -1,9 +1,10 @@
-const puppeteer = require('puppeteer');
+import {BASE_URL} from './constants.js';
+import puppeteer from 'puppeteer';
 
 const test = async () => {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
-    await page.goto('https://react-redux.realworld.io/#/settings?_k=g448rd');
+    await page.goto(`${BASE_URL}settings?_k=g448rd`);
     await page.click('.btn-outline-danger');
     await browser.close();
 }
