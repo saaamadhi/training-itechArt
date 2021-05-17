0ef395fba991b4a92115cb9ae55ec1997733b009
Angelina Valer'eva <mandaaarinka@mail.ru>
2021-05-17 17:12:09 +0300

    feat: add icon for edit btn

--- react/todo-list/src/components/TodoItem.js
+++ react/todo-list/src/components/TodoItem.js
@@ -3,17 +3,18 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import Button from '@material-ui/core/Button';
 import '../styles/todoitem.css';
-import editIcon from '../imgs/pencil.png';
-import {useDispatch} from 'react-redux';
-import {setChecked, dellTodo, editTodo} from '../redux/todoSlice';
-import PropTypes from 'prop-types'
+import SvgIcon from '@material-ui/core/SvgIcon';
+import { useDispatch } from 'react-redux';
+import { setChecked, dellTodo, editTodo } from '../redux/todoSlice';
+import PropTypes from 'prop-types';
+import CreateOutlinedIcon from '@material-ui/icons/CreateOutlined';
 
-const TodoItem = ({ id, title, checked}) => {
+const TodoItem = ({ id, title, checked }) => {
     const dispatch = useDispatch();
 
     const handlerEdit = (itemId) => {
         const newValue = prompt("Change value:", title);
-        if(newValue){
+        if (newValue) {
             dispatch(editTodo([itemId, newValue]));
         }
     }
@@ -22,16 +23,16 @@ const TodoItem = ({ id, title, checked}) => {
         <div className="todoContainer">
             <FormControlLabel
                 control={
-                        <Checkbox
-                            checked={checked}
-                            onChange={() => dispatch(setChecked(id))}
-                            color="secondary"
-                        />
+                    <Checkbox
+                        checked={checked}
+                        onChange={() => dispatch(setChecked(id))}
+                        color="secondary"
+                    />
                 }
                 label={title}
             />
-            <Button color="secondary" onClick={ () => handlerEdit(id) }>
-                <img className="editIcon" src={editIcon} alt="Icon for edit btn"/>
+            <Button color="secondary" onClick={() => handlerEdit(id)}>
+                <SvgIcon  component={CreateOutlinedIcon} />
             </Button>
             <Button color="secondary" onClick={() => dispatch(dellTodo(id))}>&times;</Button>
         </div>
@@ -39,7 +40,7 @@ const TodoItem = ({ id, title, checked}) => {
 }
 
 TodoItem.propTypes = {
-    id: PropTypes.array,
+    id: PropTypes.number,
     title: PropTypes.string,
     checked: PropTypes.bool
 };
