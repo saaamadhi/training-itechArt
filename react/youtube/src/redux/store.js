f9a96002121f0ed92f7b5f683b5d8df3b9b2a943
Angelina Valer'eva <mandaaarinka@mail.ru>
2021-05-18 20:34:50 +0300

    feat: add redux thunk

--- react/youtube/src/redux/store.js
+++ react/youtube/src/redux/store.js
@@ -1,7 +1,9 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import allReducers from './reducers/index';
+import thunk from 'redux-thunk';
 
 export const store = createStore(
     allReducers,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeWithDevTools(applyMiddleware(thunk))
 )
