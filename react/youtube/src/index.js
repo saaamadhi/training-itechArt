e5319d2964a24486d6554e6e65e52a540aff0395
Angelina Valer'eva <mandaaarinka@mail.ru>
2021-05-18 16:24:58 +0300

    feat: add index file

--- react/youtube/src/index.js
+++ react/youtube/src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {Provider} from 'react-redux';
+import './styles/index.css';
+import { store } from './redux/store';
 
 ReactDOM.render(
-  <App />,
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
