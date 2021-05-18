70b7921eefe3529a4dc760036e05a6f1f4b0fde2
Angelina Valer'eva <mandaaarinka@mail.ru>
2021-05-18 16:24:32 +0300

    feat: add app component

--- react/youtube/src/App.js
+++ react/youtube/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Search from './components/Search';
+import VideoList from './components/VideoList';
+import './styles/app.css';
+import youtubeIcon from './img/youtube-icon.png';
 
 export default function App() {
     return (
-        <div>
-            <h3>Hello3</h3>
+        <div className="app">
+            <div className="logo">
+                <img className="logo__icon" src={youtubeIcon} alt="Youtube icon"></img>
+                <h2 className="logo__title">Youtube video search</h2>
+            </div>
+            <Search />
+            <VideoList />
         </div>
     )
 }
