720e4e4885572f78ecbd772f446aa41f75474f94
Angelina Valer'eva <mandaaarinka@mail.ru>
2021-05-19 10:59:48 +0300

    fix: add constants for url

--- react/youtube/src/redux/actions/index.js
+++ react/youtube/src/redux/actions/index.js
@@ -1,10 +1,12 @@
+import {API_KEY, BASE_URL} from '../../constants';
+
 const saveAction = (payload) => ({
     type: 'SAVE_DATA',
     payload
 });
 
 const saveData = (input) => async(dispatch) => {
-    await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${input}&key=AIzaSyDxUuvgp6mRTgGdxpGimj_UY4uD2VYSw6w`)
+    await fetch(`${BASE_URL}${input}${API_KEY}`)
         .then(response => response.json())
         .then(json => dispatch(saveAction({arr: json.items})))
 }
