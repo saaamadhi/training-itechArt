e356b585c0f9a2b18d0dd229cc173a3c516df336
Angelina Valer'eva <mandaaarinka@mail.ru>
2021-05-20 11:49:24 +0300

    fix: fix structure, add new classes

--- react/youtube/src/components/VideoItem.js
+++ react/youtube/src/components/VideoItem.js
@@ -4,10 +4,15 @@ import '../styles/video-item.css';
 export default function VideoItem({title, channelTitle, description, preview}) {
     return (
         <div className="video-list__item">
-            <img src={preview.url} alt={`${title} prewiew`} width={preview.width} height={preview.height}/>
-            <p>{title}</p>
-            <p>{channelTitle}</p>
-            <p>{description}</p>
+            <div className="item__container">
+                <img src={preview.url} alt={`${title} prewiew`} width={preview.width} height={preview.height}/>
+                <div className="container__titles">
+                    <p className="video-title">{title}</p>
+                    <p className="channel-title">{channelTitle}</p>
+                </div>
+            </div>
+            <p className="item__desc-title">Description:</p>
+            <p className="item__desc">{description}</p>
         </div>
     )
 }
