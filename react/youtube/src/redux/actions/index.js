674fe953297c4348235ae74d49c5fd20ce7a8ac1
Angelina Valer'eva <mandaaarinka@mail.ru>
2021-05-27 10:46:57 +0300

    refactor: rename import

--- react/youtube/src/redux/actions/index.js
+++ react/youtube/src/redux/actions/index.js
@@ -1,15 +1,15 @@
-import {API_KEY, BASE_URL} from '../../constants';
-import {actionType} from '../actionTypes';
+import { API_KEY, BASE_URL } from '../../constants';
+import actionTypes from '../actionTypes';
 
 const saveAction = (payload) => ({
-    type: actionType.SAVE_DATA,
+    type: actionTypes.SAVE_DATA,
     payload
 });
 
-const saveData = (input) => async(dispatch) => {
+const saveData = (input) => async (dispatch) => {
     await fetch(`${BASE_URL}${input}${API_KEY}`)
         .then(response => response.json())
-        .then(json => dispatch(saveAction({arr: json.items})))
+        .then(json => dispatch(saveAction({ arr: json.items })))
 }
 
-export {saveData};
+export { saveData };
